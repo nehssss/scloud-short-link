ea954b0b659b2e7f70c9dbf3c4be04966761020e
nehssss <562410105@qq.com>
2023-05-07 22:44:27 -0500

    init project

---
+++ scloud-common/src/main/java/org/haitaos/App.java
@@ -0,0 +1,13 @@
+package org.haitaos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
