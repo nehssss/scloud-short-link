e59bfbd9e5f9c61b16e2881d67214eeb0870e3d2
nehssss <562410105@qq.com>
2023-05-23 23:55:25 -0500

    using redis store captcha

--- scloud-account/src/main/java/com/haitaos/controller/NotifyController.java
+++ scloud-account/src/main/java/com/haitaos/controller/NotifyController.java
@@ -2,10 +2,12 @@ package com.haitaos.controller;
 
 import com.google.code.kaptcha.Producer;
 import com.haitaos.service.NotifyService;
+import com.haitaos.util.CommonUtil;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,44 +15,59 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/account/v1")
 @Slf4j
 public class NotifyController {
-    private Producer captchaProducer;
+  private Producer captchaProducer;
 
-    private NotifyService notifyService;
+  private NotifyService notifyService;
 
-    public NotifyController(Producer captchaProducer, NotifyService notifyService) {
-        this.captchaProducer = captchaProducer;
-        this.notifyService = notifyService;
+  private StringRedisTemplate redisTemplate;
+
+  /** Captcha expiration time */
+  private static final long CAPTCHA_EXPIRATION = 1000 * 10 * 60;
+
+  public NotifyController(
+      Producer captchaProducer, NotifyService notifyService, StringRedisTemplate redisTemplate) {
+    this.captchaProducer = captchaProducer;
+    this.notifyService = notifyService;
+    this.redisTemplate = redisTemplate;
+  }
+
+  /**
+   * generate captcha
+   *
+   * @param request
+   * @param response
+   */
+  @GetMapping("captcha")
+  public void getCaptcha(HttpServletRequest request, HttpServletResponse response) {
+    String captchaText = captchaProducer.createText();
+    log.info("Captcha Content:{}", captchaText);
+
+    // store redis, set expiration time
+    this.redisTemplate
+        .opsForValue()
+        .set(getCaptchaKey(request), captchaText, CAPTCHA_EXPIRATION, TimeUnit.MILLISECONDS);
+
+    BufferedImage bufferedImage = captchaProducer.createImage(captchaText);
+    try (ServletOutputStream outputStream = response.getOutputStream()) {
+      ImageIO.write(bufferedImage, "jpg", outputStream);
+      outputStream.flush();
+    } catch (IOException e) {
+      log.error("Get stream error:{}", e.getMessage());
     }
+  }
 
-    /**
-     * generate captcha
-     * @param request
-     * @param response
-     */
-    @GetMapping("captcha")
-    public void getCaptcha(HttpServletRequest request, HttpServletResponse response) {
-        String captchaText = captchaProducer.createText();
-        log.info("验证码内容:{}", captchaText);
+  private String getCaptchaKey(HttpServletRequest httpServletRequest) {
+    String ip = CommonUtil.getIpAddr(httpServletRequest);
+    String userAgent = httpServletRequest.getHeader("User-Agent");
 
-        //store redis, set expiration time TODO
-
-        BufferedImage bufferedImage = captchaProducer.createImage(captchaText);
-        try {
-            ServletOutputStream outputStream = response.getOutputStream();
-            ImageIO.write(bufferedImage, "jpg", outputStream);
-            outputStream.flush();
-            outputStream.close();
-        } catch (IOException e) {
-            log.error("Get stream error:{}", e.getMessage());
-        }
-    }
-
-
+    String key = "account-service:captcha:" + CommonUtil.MD5(ip + userAgent);
+    log.info("Captcha key:{}", key);
+    return key;
+  }
 }
-
-
