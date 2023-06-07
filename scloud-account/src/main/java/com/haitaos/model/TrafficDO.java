e5195ecc1322f8061db24bb94c6e1051c513a123
nehssss <562410105@qq.com>
2023-06-06 23:45:46 -0500

    using snowfalke generate ID

--- scloud-account/src/main/java/com/haitaos/model/TrafficDO.java
+++ scloud-account/src/main/java/com/haitaos/model/TrafficDO.java
@@ -15,7 +15,7 @@ public class TrafficDO implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
-  @TableId(value = "id", type = IdType.AUTO)
+  //@TableId(value = "id", type = IdType.AUTO)
   private Long id;
 
   /** Number of short links per day limit */
